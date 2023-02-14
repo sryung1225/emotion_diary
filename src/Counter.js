516614158b4746343ae0b120c5dbd406b4434ff1
sryung <sryung1225@gmail.com>
2023-02-15 00:19:31 +0900

    [Add] Props 이해하기 + 홀짝구분 OddEvenResult.js 만들어보기 : Counter에서 OddEvenResult에 동적인 데이터 count 내려주기

--- src/Counter.js
+++ src/Counter.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react';
+import OddEvenResult from './OddEvenResult';
 
-// const Counter = (props) => {
-//   console.log(props); // 부모 컴포넌트(App)으로 받은 값인 props 확인
-//   const [count, setCount] = useState(props.initialValue); // 점 표기법으로 props 가져옴
-
-const Counter = ({ initialValue }) => { // 비구조화 할당을 통해 props 라는 객체에서 특정 값만 받는 것도 가능
-  console.log(initialValue);
+const Counter = ({ initialValue }) => {
   const [count, setCount] = useState(initialValue);
+
   const onIncrease = () => {
     setCount(count + 1);
   };
@@ -19,12 +16,14 @@ const Counter = ({ initialValue }) => { // 비구조화 할당을 통해 props �
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
+      {/* 자식 컴포넌트에게 정적인 데이터 뿐만 아니라 동적인 데이터도 전달할 수 있다 */}
+      {/* 리액트에서 동적인 데이터 중 대장은 바로 state */}
+      {/* 자식 컴포넌트 OddEvenResult 에게 count를 전달해보자 */}
+      <OddEvenResult count={count} />
     </div>
   );
 };
 
-// 만일 부모 컴포넌트에서 initialValue를 실수로 깜빡하고 내려주지 않은 경우를 방어 하기 위해
-// 자식 컴포넌트에서도 기본값을 지정해줄 수 있다
 Counter.defaultProps = {
   initialValue: 0
 }
