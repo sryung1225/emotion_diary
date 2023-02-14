cd2aee98dd6da76099c8bc7f44bece272234889b
sryung <sryung1225@gmail.com>
2023-02-14 20:49:38 +0900

    [Add] 컴포넌트 MyFooter.js 만들어서 최상위 컴포넌트 App.js을 대신해보기 다만 리액트앱에서는 최상위 컴포넌트를 App이라 부르는게 관례이기 때문에 거스를 필요는 없음 (원복 예정)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import MyFooter from './MyFooter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MyFooter />
   </React.StrictMode>
 );
 
