21c04bb9d430bc72ddffe5301e4a975738487d78
sryung <sryung1225@gmail.com>
2023-02-14 20:19:22 +0900

    [Add] 컴포넌트 MyHeader.js 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MyHeader from './MyHeader'
 
 function App() {
   let name = 'Eve';
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트 {name}</h2>
       </header>
