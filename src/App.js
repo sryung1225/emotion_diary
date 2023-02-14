f433124f86993cbb3f1bde150e14db0420edf547
sryung <sryung1225@gmail.com>
2023-02-14 22:22:47 +0900

    [Fix] 최상위 태그 원복 + JSX의 스타일링 방법 적용 1. css 스타일링(App.css) 2. 인라인 스타일링(const style)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react"
-import './App.css';
+// import './App.css';
+
 import MyHeader from './MyHeader'
 
 function App() {
   let name = 'Eve';
+
+  // css 파일 사용 없이 인라인으로 스타일 넣기도 가능함
+  const style = {
+    App: {
+      backgroundColor: 'black',
+    },
+    h2: {
+      color: 'red'
+    },
+    bold_text: {
+      color: 'green',
+    },
+  };
   return (
-    <React.Fragment>
+    // class가 자바스크립트 예약어이기 때문에 className을 이용함
+    <div className="App" style={style.App}>
       <MyHeader />
-      <header className="App-header">
-        <h2>안녕 리액트 {name}</h2>
-      </header>
-    </React.Fragment>
+      <h2 style={style.h2}>안녕 리액트 {name}</h2>
+      <b style={style.bold_text} id="bold_text">React.js</b>
+    </div>
     // JSX 규칙 1 : 닫는 태그 꼭 사용하기
     // ❌ <image> ⭕ <image /> (self closing tag)
     // JSX 규칙 2 : 최상위 태그를 사용해서 다른 모든 태그를 꼭 묶어주기 (must have one parent element)
