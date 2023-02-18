f100c8034825d7ef9c45ab0081a458d169a8393c
sryung <sryung1225@gmail.com>
2023-02-19 05:26:03 +0900

    [Fix] 오늘의 일기 + 일기 리스트 연동 - 일기 수정 2단계 - isEdit + 삼항연산자 : 조건데 따라 렌더링할 DOM 수정 - content의 state인 localContent 설정

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -12,6 +12,8 @@ const DiaryItem = ({
   const [isEdit, setIsEdit] = useState(false); // isEdit의 기본값이 false
   const toggleIsEdit = () => setIsEdit(!isEdit); // toggleIsEdit이 호출되는 순간 원래 isEdit이 갖고 있던 값을 반전
 
+  const [localContent, setLocalContent] = useState("");
+
   const handleRemove = () => {
     if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
       onRemove(id);
@@ -25,9 +27,29 @@ const DiaryItem = ({
         <br />
         <span className="date">{new Date(create_date).toLocaleString()}</span> {/* ms => 인간이 알아볼 수 있는 시간 */}
       </div>
-      <div className="content">{content}</div>
-      <button onClick={handleRemove}>삭제하기</button>
-      <button onClick={toggleIsEdit}>수정하기</button>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <>
+          <button onClick={toggleIsEdit}>수정 취소</button>
+          <button>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
