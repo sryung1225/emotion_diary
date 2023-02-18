806b58f319754fda2cfaf2da486d26b85b7c6bff
sryung <sryung1225@gmail.com>
2023-02-19 03:01:41 +0900

    [Add] 일기 리스트 만들기 1단계 - 임시 데이터(dummyList)를 이용해서 렌더링 연습해보기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+// DiaryList에 prop으로 임시 데이터 전달해보기 연습
+const dummyList = [
+  {
+    id: 1, // 모든 데이터는 고유한 id를 갖는다
+    author: "이성령",
+    content: "오늘은 몰래 막창을 시켰다. 짜릿했다.",
+    emotion: 5,
+    create_date: new Date().getTime() // 일기를 작성하는 시점을 담은 데이터 : new Date()로 현재시간 표기 후 ms로 변환
+  }, {
+    id: 2,
+    author: "이브",
+    content: "오늘은 카페라떼를 시켰다. 냠",
+    emotion: 4,
+    create_date: new Date().getTime()
+  }, {
+    id: 3,
+    author: "성령",
+    content: "카페모카는 사랑이다",
+    emotion: 5,
+    create_date: new Date().getTime()
+  }, {
+    id: 4,
+    author: "령",
+    content: "리액트 공부.. 망했다",
+    emotion: 2,
+    create_date: new Date().getTime()
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ src/DiaryList.js
@@ -0,0 +1,27 @@
+/* "일기 리스트" 만들기
+
+목표 : React에서 리스트 렌더링 하기
+- 배열을 이용하여 React에서 LIST를 렌더링 해보고 개별적인 컴포넌트로 만들어보기
+*/
+
+const DiaryList = ({ diaryList }) => {
+  console.log(diaryList); // ? (4) [{...}, {...}, {...}, {...}]
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          // <div>일기 아이템</div>
+          <div>
+            <div>작성자 : {it.author}</div>
+            <div>일기 : {it.content}</div>
+            <div>감정 : {it.emotion}</div>
+            <div>작성 시간(ms) : {it.create_date}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default DiaryList;
