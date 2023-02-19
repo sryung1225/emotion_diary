69573216402f8e13e9df7ab0eff0ea57e8f9e7d1
sryung <sryung1225@gmail.com>
2023-02-20 02:10:23 +0900

    [Fix] React Lifecycle 제어하기 2단계 - Mount 되는 시점 제어하기 - Update 되는 시점 제어하기 - useEffect 더 이해하기 : Deps가 변화하는 경우 확인하기

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -6,6 +6,35 @@ const Lifecycle = () => {
 
   const [count, setCount] = useState(0);
   const [text, setText] = useState("");
+
+  // Lifecycle "Mount" 시점에 뭘 하고 싶은 경우?
+  // - Deps에 빈배열 전달
+  // - 콜백함수에 하고 싶은 일 전달
+  useEffect(() => {
+    console.log("Mount!");
+  }, []);
+
+  // Lifecycle "Update" 시점에 뭘 하고 싶은 경우?
+  // - Deps를 전달하지 않음
+  // - 콜백함수에 하고 싶은 일 전달
+  useEffect(() => {
+    console.log("Update!");
+  });
+
+  // Deps에 있는 값이 변화하게 되면 그 순간 콜백함수가 실행됨
+  // count state가 변화하는 순간 감지 하는 예제
+  useEffect(() => {
+    console.log(`count is update : ${count}`);
+    if (count > 5) {
+      alert("count가 5를 넘었습니다 따라서 1로 초기화합니다.");
+      setCount(1);
+    }
+  }, [count]);
+  // text state가 변화하는 순간 감지 하는 예제
+  useEffect(() => {
+    console.log(`text is update : ${text}`);
+  }, [text]);
+
   return (
     <div style={{ padding: 20 }}>
       <div>
