5d8afeca98d98c4624d0fd69e60ba14ac8c9fb81
sryung <sryung1225@gmail.com>
2023-03-03 19:31:35 +0900

    [Add] React Router 예시 - 4개 페이지 작성 - React Router 기본 셋팅 - 새로고침하며 이동하지 않도록(MPA의 특징) a링크 이동 대신 React Router 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import RouteTest from './components/RouteTest';
+
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        {/* <a href="/new">New로 이동</a> 은 새로고침 발생 : MPA 방식 */}
+        <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
