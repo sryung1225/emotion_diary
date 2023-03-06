dc2a93d83e65531f507d2a747c4c6decc74716a1
sryung <sryung1225@gmail.com>
2023-03-06 16:16:21 +0900

    Router Advanced - Page Moving : useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,9 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   const id = searchParams.get("id");
   console.log("id : ", id);
@@ -17,6 +18,12 @@ const Edit = () => {
       <button onClick={() => setSearchParams({ who: "sryung" })}>
         QS바꾸기
       </button> {/* 버튼 클릭시 url이 edit?who=sryung */}
+      <button onClick={() => navigate("/home")}>
+        HOME으로 가기
+      </button> {/* Link를 클릭하지 않았을 때도 의도적으로 이동 */}
+      <button onClick={() => navigate(-1)}>
+        뒤로 가기
+      </button> {/* Link를 클릭하지 않았을 때도 의도적으로 이동 */}
     </div>
   )
 }
