6b423dcfb34d3c0881aaa1fc6652edda3252b5bb
sryung <sryung1225@gmail.com>
2023-03-07 02:38:38 +0900

    [Add] 프로젝트 기초공사 1단계 - RouteTest.js 격리 - 폰트 설정 : 구글폰트 - 감정이모지 이미지 설정 - 기본 레이아웃 - 공통 컴포넌트 설정 : 버튼 + 헤더

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import RouteTest from './components/RouteTest';
-
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+// COMPONENTS
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"좌측버튼"} onClick={() => alert("좌측버튼 클릭")} />
+          }
+          rightChild={
+            <MyButton text={"우측버튼"} onClick={() => alert("우측버튼 클릭")} />
+          }
+        />
         <h2>App.js</h2>
+        {/* 이미지 확인
+        <img src={process.env.PUBLIC_URL + `assets/emotion1.png`} />
+        */}
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"skfshkfshfje"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        {/* <a href="/new">New로 이동</a> 은 새로고침 발생 : MPA 방식 */}
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
