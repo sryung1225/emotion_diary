3e42f0bd51f1090d01cf2b61d4481f8ee268d240
sryung <sryung1225@gmail.com>
2023-03-07 02:32:43 +0900

    [Add] React Router - Query String : useSearchParams - url를 통해 자동으로 매핑되는 query문을 이용하게 해주는 Hooks useSearchParams

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id : ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode : ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정 페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "sryung" })}>
+        QS바꾸기
+      </button> {/* 버튼 클릭시 url이 edit?who=sryung */}
     </div>
   );
 };
