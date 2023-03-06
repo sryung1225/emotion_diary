e09aaf7b6c73c05b2a26b46a698524fc1a9c2c40
sryung <sryung1225@gmail.com>
2023-03-06 15:37:44 +0900

    Router basic

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+import RouteTest from './components/RouteTest';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/new" element={<New />}></Route>
+          <Route path="/edit" element={<Edit />}></Route>
+          <Route path="/diary" element={<Diary />}></Route>
+        </Routes>
+        {/* <a href="/new">New로 이동</a> */}
+        <RouteTest />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/New.js
@@ -0,0 +1,10 @@
+const New = () => {
+  return (
+    <div>
+      <h1>NEW</h1>
+      <p>이곳은 알기 작성 페이지 입니다.</p>
+    </div>
+  )
+}
+
+export default New;
