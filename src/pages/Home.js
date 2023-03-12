177747d52ded0aff571b862f7f95fb7681b5c16b
sryung <sryung1225@gmail.com>
2023-03-12 19:34:38 +0900

    [Add] Header : 현재시간 / 전.후 이동 - 홈에서 헤더 추가 - new Date() 이해

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,34 @@
+import { useState } from "react";
+
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+
 const Home = () => {
+
+  const [curDate, setCurDate] = useState(new Date()); // 현재시간 저장
+  console.log(curDate);
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+  // getMoth()는 특이하게 0~11로 계산되므로, 우리는 원하는 바를 위해 1씩 더해줘야 함
+
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  }
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  }
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>이곳은 홈 입니다.</p>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+      />
     </div>
   );
 };
