e38ffceda11ba547702f26296585732c32f4a559
sryung <sryung1225@gmail.com>
2023-03-13 02:47:32 +0900

    [Add] DiaryEditor 오늘의 감정 - 감정 객체로 저장 : emotionList - 감정을 클릭하면, 어떤 감정을 선택했는지 state로 저장 필요 : EmotionItem 컴포넌트 분류 - 기본 스타일링

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -3,10 +3,42 @@ import { useNavigate } from "react-router-dom";
 
 import MyHeader from "./MyHeader";
 import MyButton from './MyButton';
+import EmotionItem from "./EmotionItem";
 
 const getStringDate = (date) => { // date 객체를 전달 받고
   return date.toISOString().slice(0, 10); // 사용 가능하게 가공한 뒤 YYYY-MM-DD까지만 자름
 }
+
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: '최고임'
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: '좋음'
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: '그럭저럭'
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: '나쁨'
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: '끔찍함'
+  },
+]; // 감정 데이터
 
 const DiaryEditor = () => {
   const navigate = useNavigate();
@@ -30,6 +62,17 @@ const DiaryEditor = () => {
               value={date}
               onChange={(e) => setDate(e.target.value)}
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) =>
+              // <div key={it.emotion_id}>
+              //   {it.emotion_descript}
+              // </div>
+              <EmotionItem key={it.emotion_id} {...it} />
+            )}
           </div>
         </section>
       </div>
---
+++ src/components/EmotionItem.js
@@ -0,0 +1,14 @@
+const EmotionItem = ({
+  emotion_id,
+  emotion_img,
+  emotion_descript
+}) => {
+  return (
+    <div className="EmotionItem">
+      <img src={emotion_img} />
+      <span>{emotion_descript}</span>
+    </div>
+  )
+};
+
+export default EmotionItem;
