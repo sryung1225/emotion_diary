425ea049e21e8e58f7a4408fe20eb28f2c153bf8
sryung <sryung1225@gmail.com>
2023-03-12 19:56:20 +0900

    [Add] 23년 3월에는 3월 일기들만 들고오도록, 데이터 가공 - 가공된 데이터는 state로 관리 - useEffect로 특정년/월 일기만 가져오도록 관리 - 다이어리 리스트가 0개일 때는 굳이 하지 않도록 조건 사용

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 
 import MyHeader from './../components/MyHeader';
 import MyButton from './../components/MyButton';
@@ -8,11 +8,35 @@ const Home = () => {
 
   const diaryList = useContext(DiaryStateContext);
 
-  const [curDate, setCurDate] = useState(new Date()); // 현재시간 저장
-  console.log(curDate);
+  const [data, setData] = useState([]);
 
+  const [curDate, setCurDate] = useState(new Date()); // 현재시간 저장
+  // console.log(curDate);
   const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
   // getMoth()는 특이하게 0~11로 계산되므로, 우리는 원하는 바를 위해 1씩 더해줘야 함
+
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1 // 이번년도 이번월 1일
+      ).getTime();
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0 // 이번년도 이번월 마지막일 (31/30일 구분 없이 편하게 계산)
+      ).getTime();
+      // console.log(new Date(firstDay), new Date(lastDay)); // 확인
+
+      // n년 n월 에 해당되는 일기들만 뽑아냄
+      setData(diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay));
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   const increaseMonth = () => {
     setCurDate(
