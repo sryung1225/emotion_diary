d928f8f591b9562fc14ab7049443b7cb906fa106
sryung <sryung1225@gmail.com>
2023-03-13 02:14:27 +0900

    [Add] New : 오늘은 언제인가요? date input - input으로 입력받은 value를 사용하기

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import MyHeader from "../components/MyHeader";
 import MyButton from './../components/MyButton';
 
+const getStringDate = (date) => { // date 객체를 전달 받고
+  return date.toISOString().slice(0, 10); // 사용 가능하게 가공한 뒤 YYYY-MM-DD까지만 자름
+}
+
 const New = () => {
   const navigate = useNavigate();
+
+  // console.log(getStringDate(new Date()));
+  const [date, setDate] = useState(getStringDate(new Date()));
+
   return (
     <div>
       <MyHeader
         headText={"새 일기쓰기"}
         leftChild={<MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />}
       />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
